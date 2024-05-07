ab0eb2849d0f3ce6572e2c2332eb793c84665061
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-05-07 18:18:16 +0000

    end of kilgore's tutorial

--- script.js
+++ script.js
@@ -21,19 +21,40 @@ teamMembers.forEach(member => {
     const card = document.createElement('div')
     card.classList.add('col-md-4')
 
+//style the background color of the card based off position
+let backgroundColor
+
+switch(member.position.toLowerCase()){
+    case 'forward':
+        backgroundColor = "red"
+        break
+        case 'mid':
+        backgroundColor = "green"
+        break
+        case 'defender':
+        backgroundColor = "blue"
+        break
+        case '':
+        backgroundColor = "gray"
+        break
+}
+card.style.backgroundColor = backgroundColor
+
+
     card.innerHTML= `
     <div class="card">
         <div class = "card-header">
         ${member.name}
         </div>
-        <div class="card-body">
+        <div class="card-body" style="background-color:${backgroundColor};">
         <p><strong>Position:</strong> ${member.position}</p>
         </div>
     </div>
-
+  
     `
     teamCardsContainer.appendChild(card)
 });
+
 }
 
 
