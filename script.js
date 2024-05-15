49563d3d73dea8e92ffd8b69a8ca553886fb7719
SophieHornik <144719454+SophieHornik@users.noreply.github.com>
2024-05-15 17:47:07 +0000

    5/15

--- script.js
+++ script.js
@@ -9,9 +9,18 @@ const teamMembers = [
     movies:['Revenge of the Sith','A New Hope','Empire Strikes Back','Return of the Jedi'],
     biography:'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.i is cool',
     img:'vader.png'
+},
+{
+    name: 'Darth Vader',
+    species: 'Human',
+    alive: false,
+    saberColor: 'red',
+    children:['Luke Skywalker','Leia Organa (Skywalker)'],
+    movies:['Revenge of the Sith','A New Hope','Empire Strikes Back','Return of the Jedi'],
+    biography:'Darth Vader, born as Anakin Skywalker, was a central figure in the Star Wars saga. Initially a heroic Jedi Knight, he fell to the dark side of the Force due to fear, anger, and manipulation by Emperor Palpatine. Clad in menacing black armor and wielding a red lightsaber, Vader became a symbol of tyranny in the galaxy. Despite his villainous deeds, a flicker of redemption remained within him, ultimately leading to his self-sacrifice to save his son, Luke Skywalker, and destroy the Emperor, bringing balance to the Force.i is cool',
+    img:'vader.png'
 }
 ]
-
 
 function generateTeamCards(){
 
@@ -62,7 +71,7 @@ card.style.backgroundColor = backgroundColor
 
 
     card.innerHTML= `
-    <div class="card mt-5">
+    <div class="card m-5">
         <div class = "card-header text-center">
         <img src="${member.img}" class="img-fluid card-img">
       ${member.name}
@@ -80,8 +89,5 @@ card.style.backgroundColor = backgroundColor
     
     teamCardsContainer.appendChild(card)
 });
-
 }
-
-
 window.onload = generateTeamCards()
